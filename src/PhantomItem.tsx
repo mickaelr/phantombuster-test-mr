53c84152378dfaba015aaaf1cb10cc0590f72700
Mickael Roy <roy-mickael@live.fr>
2024-03-15 23:35:41 +0100

    fetch phantoms data from json file (should we use a fakeAPI service like https://mocki.io/fake-json-api? Also create basic components to display Phantoms (and update the type accordingly

---
+++ src/PhantomItem.tsx
@@ -0,0 +1,11 @@
+import { IPhantom } from './phantom'
+
+function PhantomItem(props: IPhantom) {
+    return (
+      <div>
+        <div>{ props.name }</div>
+      </div>
+    )
+  }
+  
+  export default PhantomItem
---
+++ src/PhantomList.tsx
@@ -0,0 +1,17 @@
+import PhantomItem from './PhantomItem'
+import { IPhantom } from './phantom'
+
+function PhantomList(props: { items: IPhantom[] }) {
+  
+    return (
+      <div>
+        <div>
+          { props.items.map((phantom, index) => (
+            <PhantomItem key={index} {...phantom} />
+          ))}
+        </div>
+      </div>
+    )
+  }
+  
+  export default PhantomList
