6b53f779982351c4555319c219f3ccb1e115f33c
Mickaël Roy <roy-mickael@live.fr>
2024-03-17 15:46:06 +0100

    add story for Dropdown menu
    also add an example of interactions testing

---
+++ src/common/DropdownMenu.stories.ts
@@ -0,0 +1,36 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import DropdownMenu from './DropdownMenu';
+import { within, userEvent, expect } from '@storybook/test';
+
+const meta = {
+  title: 'Common/DropdownMenu',
+  component: DropdownMenu,
+  parameters: {
+    layout: 'centered',
+  },
+  tags: ['autodocs'],
+} satisfies Meta<typeof DropdownMenu>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const SimpleMenu: Story = {
+  args: {
+    items: [
+      { text: 'First action', action: () => { alert('First action clicked') } },
+      { text: '2nd action', action: () => { alert('2nd action clicked') } }
+    ],
+    children: 'Menu'
+  },
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+    const menuButton = canvas.getByRole('button', { name: /Menu/i });
+    await expect(menuButton).toBeInTheDocument();
+    await userEvent.click(menuButton);
+
+    const firstMenuItem = canvas.getByRole('button', { name: /First action/i });
+    const secondMenuItem = canvas.getByRole('button', { name: /2nd action/i });
+    await expect(firstMenuItem).toBeInTheDocument();
+    await expect(secondMenuItem).toBeInTheDocument();
+  },
+};
